c71464071282ff6d82f48f8396d464df27894831
Phil <phillippelliott@gmail.com>
2022-11-16 11:21:56 +0700

    cleaned up some typescript

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89f875a17f1029fa"; }
+/******/ 		__webpack_require__.h = function() { return "67bd1790f5f2660e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
